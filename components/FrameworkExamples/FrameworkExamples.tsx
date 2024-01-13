b2d1e4e05be1cf6b9aa3a35ab37abe2a74bd4ab9
Lluis Agusti <hi@llu.lu>
2024-01-13 19:02:27 +0100

    chore: homepage

---
+++ components/FrameworkExamples/FrameworkExamples.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { addClassToHast, codeToHtml } from "shikiji";
+import { FrameWork, frameworkDetails } from "utils/frameworks";
+import { FileCode } from "@phosphor-icons/react";
+import styles from "./styles.module.css";
+
+async function renderNextJs(framework: FrameWork) {
+  return codeToHtml(frameworkDetails[framework].code, {
+    lang: "typescript",
+    theme: "material-theme-darker",
+    transformers: [
+      {
+        pre(node) {
+          addClassToHast(node, `!p-4 rounded-lg ${styles["example-height"]}`);
+        },
+      },
+    ],
+  });
+}
+
+export function FrameworkExamples() {
+  const [active, setActive] = useState<FrameWork>(FrameWork.NextJs);
+  const [example, setExample] = useState("");
+
+  useEffect(() => {
+    renderNextJs(active).then((code) => setExample(code));
+  }, [active]);
+
+  return (
+    <div className="w-full flex flex-row flex-wrap max-w-5xl items-start justify-between h-">
+      <div className="flex flex-row flex-wrap justify-start items-center gap-4 w-1/3 mx-auto m-0">
+        {Object.keys(frameworkDetails).map((f: FrameWork) => {
+          const desc = frameworkDetails[f];
+          return (
+            <button
+              onClick={() => setActive(f)}
+              className="flex w-32 h-32 flex-col flex-wrap gap-2 items-center justify-center rounded-lg border-4 border-solid border-slate-200 p-4 aria-selected:border-indigo-400"
+              aria-selected={f === active}
+            >
+              <img src={desc.logo} width={desc.logoW} />
+              <div className="m-0 text-sm font-bold">{desc.title}</div>
+            </button>
+          );
+        })}
+      </div>
+      <aside className="!w-2/3">
+        <div
+          className="rounded-lg m-0 mb-2"
+          dangerouslySetInnerHTML={{ __html: example }}
+        />
+        <a
+          className="w-full flex flex-row items-center gap-2 justify-end cursor-pointer"
+          target="_blank"
+          href={frameworkDetails[active].example}
+        >
+          <FileCode fontSize="2rem" className="inline" />
+          <span>
+            Check the{" "}
+            <span className="no-underline text-purple-700 font-medium">
+              {frameworkDetails[active].title} example app
+            </span>
+          </span>
+        </a>
+      </aside>
+    </div>
+  );
+}
---
+++ utils/frameworks.ts
@@ -0,0 +1,91 @@
+type Details = {
+  title: string;
+  code: string;
+  logo: string;
+  logoW: string;
+  example: string;
+};
+
+export enum FrameWork {
+  NextJs = "nextjs",
+  Svelte = "svelte",
+  SolidStart = "solidstart",
+  // Express = "express",
+}
+
+export const frameworkDetails: Record<FrameWork, Details> = {
+  [FrameWork.NextJs]: {
+    title: "Next.js",
+    code: codeNextJs(),
+    logo: "https://authjs.dev/img/frameworks/nextjs.svg",
+    logoW: "45",
+    example: "https://next-auth-example.vercel.app/",
+  },
+  [FrameWork.Svelte]: {
+    title: "Sveltekit",
+    code: codeSvelte(),
+    logo: "https://authjs.dev/img/frameworks/sveltekit.svg",
+    logoW: "40",
+    example: "https://sveltekit-auth-example.vercel.app/",
+  },
+  [FrameWork.SolidStart]: {
+    title: "SolidStart",
+    code: codeSolid(),
+    logo: "https://authjs.dev/img/frameworks/solidstart.svg",
+    logoW: "45",
+    example: "https://auth-solid.vercel.app/",
+  },
+};
+
+/**
+ * Using functions for these to not have to have them at the top of the file and block readability...
+ */
+function codeNextJs() {
+  return `
+// auth.ts
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+export const { auth, handlers } = NextAuth({ providers: [ GitHub ] })
+  
+// middleware.ts
+export { auth as default } from "auth"
+  
+// app/api/auth/[...nextauth].ts
+import { handlers } from "auth"
+export const { GET, POST } = handlers
+export const runtime = "edge"
+`;
+}
+
+function codeSvelte() {
+  return `
+import { SvelteKitAuth } from "@auth/sveltekit"
+import GitHub from '@auth/sveltekit/providers/github'
+import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+  
+export const handle = SvelteKitAuth({
+  providers: [
+    GitHub({
+      clientId: GITHUB_ID,
+      clientSecret: GITHUB_SECRET
+    })
+  ],
+})
+`;
+}
+
+function codeSolid() {
+  return `
+import { SolidAuth } from "@auth/solid-start"
+import GitHub from "@auth/core/providers/github"
+  
+export const { GET, POST } = SolidAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
+  ]
+})
+`;
+}
