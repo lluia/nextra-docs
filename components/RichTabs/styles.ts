4d3e9ffabbcddcc6014115ecd4e0d2f036f44966
Lluis Agusti <hi@llu.lu>
2024-01-18 22:07:42 +0100

    chore: updates

--- components/RichTabs/styles.ts
+++ components/RichTabs/styles.ts
@@ -1,7 +1,7 @@
 export const styles = {
   root: "px-0 pt-4 m-0 rounded-lg mt-2",
   tablist: "flex flex-row items-center justify-start gap-8 p-0 -mb-1px",
-  tab: "relative font-semibold bg-slate-50 text-sm border-l border-t border-r border-solid border-slate-200 rounded-tl-lg rounded-tr-lg flex flex-col items-center justify-between w-48 h-28 aria-selected:bg-white aria-selected:border-b-white aria-selected:top-px",
+  tab: "relative font-semibold bg-slate-50 text-sm border-l border-t border-r border-solid border-slate-200 rounded-tl-lg rounded-tr-lg flex flex-col items-center justify-between w-40 h-24 aria-selected:bg-white aria-selected:border-b-white aria-selected:top-px",
   panels: "p-0 m-0",
   panel:
     "px-8 py-8 border border-solid border-slate-200  rounded-bl-lg rounded-br-lg rounded-tr-lg shadow-sm",
