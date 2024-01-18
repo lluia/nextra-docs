b805ac6e59130dd39cb1bee1525282e3e9cf1fbc
ndom91 <yo@ndo.dev>
2024-01-18 23:10:37 +0100

    fix: simplify tabs and dark mode

--- components/RichTabs/index.tsx
+++ components/RichTabs/index.tsx
@@ -1,39 +1,56 @@
 import { useEffect, useState } from "react";
-import * as RadixTabs from "@radix-ui/react-tabs";
+import { List, Trigger, Root, Content } from "@radix-ui/react-tabs";
+import type {
+  TabsListProps,
+  TabsTriggerProps,
+  TabsContentProps,
+  TabsProps,
+} from "@radix-ui/react-tabs";
 import cx from "classnames";
-import { styles } from "./styles";
 import { useRouter } from "next/router";
 
-RichTabs.TabList = function TabList({
-  className,
-  ...p
-}: RadixTabs.TabsListProps) {
-  return <RadixTabs.List {...p} className={cx(styles.tablist, className)} />;
+RichTabs.List = ({ className, ...rest }: TabsListProps) => {
+  return (
+    <List
+      {...rest}
+      className={cx(
+        "flex flex-row items-center justify-start gap-8 p-0 -mb-1px",
+        className
+      )}
+    />
+  );
 };
 
-RichTabs.Tab = function Tab({ className, ...p }: RadixTabs.TabsTriggerProps) {
-  return <RadixTabs.Trigger {...p} className={cx(styles.tab, className)} />;
+RichTabs.Trigger = ({ className, ...rest }: TabsTriggerProps) => {
+  return (
+    <Trigger
+      {...rest}
+      className={cx(
+        "relative font-semibold dark:bg-neutral-900 bg-slate-50 text-sm border-l border-t border-r border-solid dark:border-gray-800 border-slate-200 rounded-tl-lg rounded-tr-lg flex flex-col items-center justify-between w-48 h-28 dark:aria-selected:bg-neutral-700 transition-all duration-300 aria-selected:bg-white aria-selected:border-b-white aria-selected:top-px",
+        className
+      )}
+    />
+  );
 };
 
-RichTabs.Panels = function Panels({
-  className,
-  ...p
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return <div {...p} className={cx(styles.panels, className)} />;
-};
-
-RichTabs.Panel = function Panel({
-  className,
-  ...p
-}: RadixTabs.TabsContentProps) {
-  return <RadixTabs.Content {...p} className={cx(styles.panel, className)} />;
+RichTabs.Content = ({ className, ...rest }: TabsContentProps) => {
+  return (
+    <Content
+      {...rest}
+      className={cx(
+        "px-8 py-8 border border-solid dark:border-gray-800 border-slate-200  rounded-bl-lg rounded-br-lg rounded-tr-lg shadow-sm",
+        className
+      )}
+    />
+  );
 };
 
 export function RichTabs({
   children,
   className,
+  orientation = "horizontal",
   ...rest
-}: RadixTabs.TabsProps) {
+}: TabsProps) {
   let [tabValue, setTabValue] = useState(rest.defaultValue);
   const router = useRouter();
   const {
@@ -55,13 +72,14 @@ export function RichTabs({
   }
 
   return (
-    <RadixTabs.Root
-      className={cx(styles.root, className)}
+    <Root
+      className={cx("px-0 pt-4 m-0 rounded-lg mt-2", className)}
+      orientation={orientation}
       {...rest}
       value={tabValue}
       onValueChange={handleValueChanged}
     >
       {children}
-    </RadixTabs.Root>
+    </Root>
   );
 }
