4d622d7210aab8bcc8e08f0758e867f1d4bf6478
ndom91 <yo@ndo.dev>
2024-01-18 16:15:40 +0100

    fix: screenshot component tweaks

--- components/Screenshot/index.tsx
+++ components/Screenshot/index.tsx
@@ -1,12 +1,12 @@
-import cx from 'classnames'
-import Image from 'next/image'
+import cx from "classnames";
+import Image from "next/image";
 
 export function Screenshot({ src, alt, full, className }) {
   return (
     <div
       className={cx(
-        'mt-6 flex justify-center overflow-hidden rounded-xl border border-zinc-200 shadow-lg',
-        full ? 'bg-white' : 'm-8 bg-zinc-100',
+        "mt-6 flex justify-center overflow-hidden rounded-xl border border-zinc-200 shadow-lg",
+        full ? "bg-white" : "m-8 bg-zinc-100",
         className
       )}
     >
@@ -14,10 +14,10 @@ export function Screenshot({ src, alt, full, className }) {
         src={src}
         alt={alt}
         className={cx(
-          'w-auto select-none bg-white',
-          full ? '' : 'ring-1 ring-gray-200'
+          "w-auto select-none bg-white",
+          full ? "" : "ring-1 ring-gray-200"
         )}
       />
     </div>
-  )
+  );
 }
