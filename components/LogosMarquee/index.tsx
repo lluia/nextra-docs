0f02b3efd24cdbe6fa597f76f0418f38bfa92793
ndom91 <yo@ndo.dev>
2024-01-19 01:53:22 +0100

    fix: cleanup logo marquee and add gradient

--- components/LogosMarquee/index.tsx
+++ components/LogosMarquee/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
+import { join } from "path";
 
 /*
  the logos have a hover effect that increases their size slightly
@@ -17,47 +18,35 @@ const calculatedLogosGap = `${logosGap - extraSpace}px`;
 
 export function LogosMarquee() {
   return (
-    <Marquee
-      className=""
-      style={{
-        padding: "70px 0px",
-        height: "236px",
-        maskImage:
-          "linear-gradient(to right, rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 15%, rgb(0, 0, 0) 85%, rgba(0, 0, 0, 0) 100%)",
-      }}
-      direction="right"
-      autoFill
-    >
-      <ul
-        className="flex flex-row justify-center items-center"
-        style={{ rowGap: calculatedLogosGap, marginRight: calculatedLogosGap }}
+    <div className="py-24 w-full">
+      <Marquee
+        direction="right"
+        gradient={true}
+        gradientWidth={300}
+        gradientColor="#171717"
+        autoFill
       >
         {imageUrls.map((url) => (
-          <li
-            className="list-none"
+          <div
             key={url}
             style={{
-              width: `${logoSize * logoMagnifyBy}px`,
-              height: `${logoSize * logoMagnifyBy}px`,
+              marginRight: "1rem",
+              width: `${logoSize}px`,
+              height: `${logoSize}px`,
+              transition: "0.2s ease-in-out",
             }}
+            className="relative select-none"
           >
-            <div
+            <img
+              className="rounded-3xl shadow-lg shadow-neutral-950"
+              src={url}
               key={url}
-              style={{
-                boxShadow:
-                  "0px 0px 10px -5px rgba(255, 255, 255, 0.14), 0px 0px 25px -5px rgba(255, 255, 255, 0.30)",
-                width: `${logoSize}px`,
-                height: `${logoSize}px`,
-                transition: "0.2s ease-in-out",
-              }}
-              className="rounded-3xl overflow-hidden relative select-none"
-            >
-              <img src={url} key={url} alt="Example Company" />
-            </div>
-          </li>
+              alt="Example Company"
+            />
+          </div>
         ))}
-      </ul>
-    </Marquee>
+      </Marquee>
+    </div>
   );
 }
 
