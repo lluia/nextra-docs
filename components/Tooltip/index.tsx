60710865962f7ae6fb827b27f20a2b3ed4c86825
ndom91 <yo@ndo.dev>
2024-01-20 12:06:51 +0100

    feat: add ark-ui tooltip fadeIn/fadeOut animation

--- components/Tooltip/index.tsx
+++ components/Tooltip/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Tooltip as ArkTooltip } from "@ark-ui/react";
-import { ChildrenProps } from "../../utils/types";
+import { Tooltip as ArkTooltip } from "@ark-ui/react/tooltip";
 
 interface Props {
   label: string;
@@ -9,10 +8,10 @@ interface Props {
 
 export function Tooltip({ label, children }: Props) {
   return (
-    <ArkTooltip.Root openDelay={0} lazyMount unmountOnExit>
+    <ArkTooltip.Root>
       <ArkTooltip.Trigger>{children}</ArkTooltip.Trigger>
       <ArkTooltip.Positioner>
-        <ArkTooltip.Content className="bg-purple-100 rounded-lg text-fuchsia-900 px-4 py-2 text-sm max-w-xs text-center shadow-md  border">
+        <ArkTooltip.Content className="py-2 px-4 max-w-xs text-sm text-center text-fuchsia-900 bg-purple-100 rounded-lg border shadow-md transition-all duration-500">
           {label}
         </ArkTooltip.Content>
       </ArkTooltip.Positioner>
