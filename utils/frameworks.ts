bf9e07826be1204dd3a797ee72246e1942bbaff9
ndom91 <yo@ndo.dev>
2024-01-22 16:22:23 +0100

    fix: cleanup homepage

--- utils/frameworks.ts
+++ utils/frameworks.ts
@@ -48,13 +48,11 @@ import GitHub from "next-auth/providers/github"
 export const { auth, handlers } = NextAuth({ providers: [ GitHub ] })
   
 // middleware.ts
-export { auth as default } from "auth"
+export { auth as default } from "@/auth"
   
-// app/api/auth/[...nextauth].ts
-import { handlers } from "auth"
-export const { GET, POST } = handlers
-export const runtime = "edge"
-`;
+// app/api/auth/[...nextauth]/route.ts
+import { handlers } from "@/auth"
+export const { GET, POST } = handlers`;
 }
 
 function codeSvelte() {
