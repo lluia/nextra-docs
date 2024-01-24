b193e1d993709f073fefb988a4617f03725c1574
Balázs Orbán <info@balazsorban.com>
2024-01-24 01:26:17 +0100

    tweak config

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -1,41 +1,33 @@
 // @ts-check
-const fs = require("node:fs")
-const path = require("node:path")
+const fs = require("node:fs");
+const path = require("node:path");
 
+const core = "../next-auth/packages/core";
 // list providers entries from @auth/core/providers/*.ts
-const coreSrc = "/opt/nextauthjs/next-auth/packages/core/src"
-// const coreSrc = "../packages/core/src"
+const coreSrc = `${core}/src`;
 const providers = fs
   .readdirSync(path.join(coreSrc, "/providers"))
   .filter((file) => file.endsWith(".ts") && !file.startsWith("oauth"))
-  .map((p) => `${coreSrc}/providers/${p}`)
+  .map((p) => `${coreSrc}/providers/${p}`);
 
-/**
- * @type {import('typedoc').TypeDocOptions & import('typedoc-plugin-markdown').MarkdownTheme}
- */
+/** @type {import('typedoc').TypeDocOptions & import('typedoc-plugin-markdown').PluginOptions} */
 module.exports = {
+  outputFileStrategy: "modules",
+  cleanOutputDir: true,
+  disableSources: true,
   entryPoints: ["index.ts", "adapters.ts", "errors.ts", "jwt.ts", "types.ts"]
     .map((e) => `${coreSrc}/${e}`)
     .concat(providers),
   entryPointStrategy: "expand",
-  out: "pages/reference/core",
-  tsconfig: "/opt/nextauthjs/next-auth/packages/core/tsconfig.json",
-  // tsconfig: "../packages/core/tsconfig.json",
-  plugin: ["typedoc-plugin-markdown", require.resolve("./typedoc-mdn-links.cjs")],
-  hideInPageTOC: true,
-  disableSources: true,
-  hideBreadcrumbs: true,
-  excludeNotDocumented: true,
   excludeExternals: true,
   excludeInternal: true,
+  excludeNotDocumented: true,
   excludePrivate: true,
   excludeProtected: true,
-  cleanOutputDir: true,
-  gitRevision: "main",
   githubPages: false,
+  gitRevision: "main",
+  hideBreadcrumbs: true,
   hideGenerator: true,
-  readme: "none",
-  sort: ["kind", "static-first", "required-first", "alphabetical"],
   kindSortOrder: [
     "Function",
     "TypeAlias",
@@ -59,4 +51,12 @@ module.exports = {
     "GetSignature",
     "SetSignature",
   ],
-}
+  plugin: [
+    "typedoc-plugin-markdown",
+    require.resolve("./typedoc-mdn-links.cjs"),
+  ],
+  readme: "none",
+  sort: ["kind", "static-first", "required-first", "alphabetical"],
+  tsconfig: `${core}/tsconfig.json`,
+  out: "pages/reference/core",
+};
