77793ee5f48d9ff37f268bb73904763104ca6511
ndom91 <yo@ndo.dev>
2024-01-26 23:59:34 +0100

    fix: only load script in Vercel prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />;
-      <Script id="clerk-twitter-pixel" strategy="lazyOnload">{`
+      {process.env.VERCEL_ENV === "production" && (
+        <Script id="clerk-twitter-pixel" strategy="lazyOnload">{`
 !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
 },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
 a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
@@ -13,6 +14,7 @@ a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,
 twq('config','o6tnh');
 twq('event', 'tw-o6tnh-oi8tp', {});
       `}</Script>
+      )}
     </>
   );
 }
