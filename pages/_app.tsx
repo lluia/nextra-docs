2cfe19619f88e8b6f4acc7e05fa9a038e5ca71a2
ndom91 <yo@ndo.dev>
2024-01-26 17:23:08 +0100

    feat: add twitter pixel to app with next/script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
 import "./global.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Script id="clerk-twitter-pixel" strategy="lazyOnload">{`
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+
+twq('config','o6tnh');
+twq('event', 'tw-o6tnh-oi8tp', {});
+      `}</Script>
+    </>
+  );
 }
