12e65d89bbabf213d6b7ddd05768c96bc013fcb8
Balázs Orbán <info@balazsorban.com>
2024-01-27 16:08:48 +0100

    fix marquee

--- components/LogosMarquee/index.tsx
+++ components/LogosMarquee/index.tsx
@@ -1,56 +1,42 @@
 import Marquee from "react-fast-marquee";
+import Img from "next/image";
+import manifest from "../../pages/data/manifest.json";
 
-/*
- the logos have a hover effect that increases their size slightly
- when using transform scale, the images become blurry (because the browser is trying to scale the image up itself)
- instead we are simply increasing the images width/height on hover
- we are also increasing the width/height of the image container to prevent the logos causing each other to move
- */
+const logoSize = 72; // px
 
-const logoSize = 96; // px
-
-export function LogosMarquee() {
+export function LogosMarquee({
+  direction = "right",
+}: {
+  direction?: "left" | "right";
+}) {
   return (
-    <div className="py-24 w-full">
+    <div className="py-2 w-full">
       <Marquee
-        direction="right"
+        speed={Math.floor(Math.random() * (50 - 30 + 1)) + 30}
+        direction={direction}
         gradient={true}
         gradientWidth={300}
         gradientColor="#171717"
         autoFill
       >
-        {imageUrls.map((url) => (
-          <div
-            key={url}
-            style={{
-              marginRight: "1rem",
-              width: `${logoSize}px`,
-              height: `${logoSize}px`,
-              transition: "0.2s ease-in-out",
-            }}
-            className="relative select-none"
-          >
-            <img
-              className="rounded-3xl shadow-lg shadow-neutral-950"
-              src={url}
-              key={url}
-              alt="Example Company"
-            />
-          </div>
-        ))}
+        {Object.entries(manifest.providers)
+          .sort(() => Math.random() - 0.5)
+          .map(([key, name]) => (
+            <div
+              key={key}
+              style={{ width: logoSize, height: logoSize }}
+              className="flex p-3 select-none mr-4 bg-white dark:bg-gray-400 rounded-3xl shadow-lg shadow-neutral-950"
+            >
+              <Img
+                className="drop-shadow-lg"
+                src={`/img/providers/${key}.svg`}
+                width={logoSize}
+                height={logoSize}
+                alt={`${name} logo`}
+              />
+            </div>
+          ))}
       </Marquee>
     </div>
   );
 }
-
-const imageUrls = [
-  "/img/providers-2/stripe.webp",
-  "/img/providers-2/spacex.webp",
-  "/img/providers-2/openai.webp",
-  "/img/providers-2/discord.webp",
-  "/img/providers-2/plaid.webp",
-  "/img/providers-2/notion.webp",
-  "/img/providers-2/linear.webp",
-  "/img/providers-2/revolut.webp",
-  "/img/providers-2/klarna.webp",
-];
