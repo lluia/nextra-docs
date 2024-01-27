47769872b6c5e5d5c2a0ec9aa20be35063637ce6
Balázs Orbán <info@balazsorban.com>
2024-01-27 11:21:35 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,14 @@
 import "./global.css";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }) {
+  if (process.env.VERCEL_ENV !== "production") {
+    return <Component {...pageProps} />;
+  }
   return (
     <>
-      <Component {...pageProps} />;
-      {process.env.VERCEL_ENV === "production" && (
-        <Script id="clerk-twitter-pixel" strategy="lazyOnload">{`
-!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-
-twq('config','o6tnh');
-twq('event', 'tw-o6tnh-oi8tp', {});
-      `}</Script>
-      )}
+      <Component {...pageProps} />
+      <GoogleTagManager gtmId="AW-11313383806" />
     </>
   );
 }
