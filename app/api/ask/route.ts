5f53b895c0af2c29bdd66d0c7d3b53a5e7bd0f63
Balázs Orbán <info@balazsorban.com>
2024-01-29 03:59:05 +0100

    feat: add ask API

---
+++ app/api/ask/route.ts
@@ -0,0 +1,22 @@
+interface Query {
+  query: string;
+}
+
+export async function POST(request: Request): Promise<Response | void> {
+  try {
+    const body: unknown = await request.json();
+    assertBody(body);
+
+    return new Response(body.query);
+  } catch (error) {
+    return new Response(error, { status: 500 });
+  }
+}
+
+function assertBody(body: unknown): asserts body is Query {
+  if (!body) throw new TypeError("Missing body");
+  if (typeof body !== "object") throw new TypeError("Body must be an object");
+  if (!("query" in body)) throw new TypeError("Missing query in body");
+  if (typeof body.query !== "string")
+    throw new TypeError("Query must be a string");
+}
