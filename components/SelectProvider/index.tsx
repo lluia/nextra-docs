97c0bd772e4f7f803f142f6a893ea5f855d37dbd
Lluis Agusti <hi@llu.lu>
2024-01-30 19:40:03 +0100

    chore: more things

--- components/SelectProvider/index.tsx
+++ components/SelectProvider/index.tsx
@@ -43,9 +43,70 @@ export function SelectProvider() {
             ))}
           </Ariakit.ComboboxPopover>
         ) : !term ? (
-          <p className="mt-6 py-2 px-4 bg-amber-100 rounded-md">
-            Type and select a provider to see its setup instructions.
-          </p>
+          <>
+            <p className=" mt-8 rounded-md">
+              <strong>
+                ↑ Type and select a provider to see its setup instructions.
+              </strong>{" "}
+              Otherwise, here are some suggested OAuth providers, click on any
+              of them to see the provider's setup instructions:
+            </p>
+            <div className="mt-8 flex flex-row gap-6 pb-8">
+              <div
+                role="button"
+                onClick={() =>
+                  handleSelectOption({ id: "google", name: "Google" })
+                }
+                className="p-4 border border-solid dark:border-neutral-800 border-slate-200 rounded-lg flex flex-col items-center justify-between w-32 shadow-lg h-32 hover:bg-slate-50 transition-colors"
+              >
+                <img src={`/img/providers/google.svg`} className="w-11 mt-2" />
+                <div className="text-sm text-center">Google</div>
+              </div>
+              <div
+                role="button"
+                onClick={() =>
+                  handleSelectOption({ id: "twitter", name: "Twitter" })
+                }
+                className="p-4 border border-solid dark:border-neutral-800 border-slate-200 rounded-lg flex flex-col items-center justify-between w-32 shadow-lg h-32 hover:bg-slate-50 transition-colors"
+              >
+                <img src={`/img/providers/twitter.svg`} className="w-11 mt-2" />
+                <div className="text-sm text-center">Twitter</div>
+              </div>
+              <div
+                role="button"
+                onClick={() =>
+                  handleSelectOption({ id: "facebook", name: "Facebook" })
+                }
+                className="p-4 border border-solid dark:border-neutral-800 border-slate-200 rounded-lg flex flex-col items-center justify-between w-32 shadow-lg h-32 hover:bg-slate-50 transition-colors"
+              >
+                <img
+                  src={`/img/providers/facebook.svg`}
+                  className="w-11 mt-2"
+                />
+                <div className="text-sm text-center">Facebook</div>
+              </div>
+              <div
+                role="button"
+                onClick={() =>
+                  handleSelectOption({ id: "auth0", name: "Auth0" })
+                }
+                className="p-4 border border-solid dark:border-neutral-800 border-slate-200 rounded-lg flex flex-col items-center justify-between w-32 shadow-lg h-32 hover:bg-slate-50 transition-colors"
+              >
+                <img src={`/img/providers/auth0.svg`} className="w-11 mt-2" />
+                <div className="text-sm text-center">Auth0</div>
+              </div>
+              <div
+                role="button"
+                className="p-4 border border-solid dark:border-neutral-800 border-slate-200 rounded-lg flex flex-col items-center justify-between w-32 shadow-lg h-32 hover:bg-slate-50 transition-colors"
+                onClick={() =>
+                  handleSelectOption({ id: "auth0", name: "Auth0" })
+                }
+              >
+                <img src={`/img/providers/github.svg`} className="w-11 mt-2" />
+                <div className="text-sm text-center">Github</div>
+              </div>
+            </div>
+          </>
         ) : (
           <p className="mt-6 py-2 px-4 bg-amber-100 rounded-md">
             Can't find the OAuth provider you're looking for? Then, you'll need
