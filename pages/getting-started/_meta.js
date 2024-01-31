e3d2d71b9f08f41b22aa6abe0d67489022e59bbe
Nico Domino <yo@ndo.dev>
2024-01-31 01:02:16 +0100

    feat: move guides back to own page and top nav bar (#38)

---
+++ pages/getting-started/_meta.js
@@ -0,0 +1,31 @@
+export default {
+  index: "Introduction",
+  "migrating-to-v5": "Migrate to NextAuth.js v5",
+  "-- getting-started": {
+    type: "separator",
+    title: "Getting Started",
+  },
+  installation: "Installation",
+  database: "Database",
+  authentication: {
+    title: "Authentication",
+    theme: {
+      toc: false,
+    },
+  },
+  "session-management": {
+    title: "Session Management",
+    theme: {
+      toc: false,
+    },
+  },
+  deployment: "Deployment",
+  typescript: "TypeScript",
+  "-- more": {
+    type: "separator",
+    title: "More",
+  },
+  integrations: "Integrations",
+  contributors: "Contributors",
+  sponsors: "Sponsors",
+};
