c60792a8241ec8367e53283dc8a64aa28644c6fa
ndom91 <yo@ndo.dev>
2024-02-01 12:59:22 +0100

    feat: updated broken-link-checker custom action

--- .github/actions/broken-link-checker/src/index.js
+++ .github/actions/broken-link-checker/src/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -35,16 +58,176 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-//// @ts-expect-error
-// import { reasons, SiteChecker } from "broken-link-checker";
-// import { setFailed } from "@actions/core";
-var _a = require("broken-link-checker"), reasons = _a.reasons, SiteChecker = _a.SiteChecker;
-var setFailed = require("@actions/core").setFailed;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// @ts-expect-error
+var broken_link_checker_1 = __importDefault(require("broken-link-checker"));
+var core_1 = require("@actions/core");
+var github = __importStar(require("@actions/github"));
+var COMMENT_TAG = "## Broken Links";
+function findBotComment(_a) {
+    var octokit = _a.octokit, owner = _a.owner, repo = _a.repo, prNumber = _a.prNumber;
+    return __awaiter(this, void 0, void 0, function () {
+        var comments, error_1;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    _b.trys.push([0, 2, , 3]);
+                    return [4 /*yield*/, octokit.rest.issues.listComments({
+                            owner: owner,
+                            repo: repo,
+                            issue_number: prNumber,
+                        })];
+                case 1:
+                    comments = (_b.sent()).data;
+                    return [2 /*return*/, comments.find(function (c) { var _a; return (_a = c.body) === null || _a === void 0 ? void 0 : _a.includes(COMMENT_TAG); })];
+                case 2:
+                    error_1 = _b.sent();
+                    (0, core_1.setFailed)("Error finding bot comment: " + error_1);
+                    return [2 /*return*/, undefined];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
+}
+function updateCheckStatus(commentUrl) {
+    return __awaiter(this, void 0, void 0, function () {
+        var checkName, summary, text, context, getOctokit, octokit, _a, owner, repo, pullRequest, sha, checkParams, error_2;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    checkName = "Broken Link Checker";
+                    summary = "This PR introduces broken links to the docs. Click details for a list.";
+                    text = "[See the comment for details](".concat(commentUrl, ")");
+                    context = github.context, getOctokit = github.getOctokit;
+                    octokit = getOctokit(process.env.GITHUB_TOKEN);
+                    _a = context.repo, owner = _a.owner, repo = _a.repo;
+                    pullRequest = context.payload.pull_request;
+                    sha = pullRequest === null || pullRequest === void 0 ? void 0 : pullRequest.head.sha;
+                    checkParams = {
+                        owner: owner,
+                        repo: repo,
+                        name: checkName,
+                        head_sha: sha,
+                        status: "completed",
+                        conclusion: "failure",
+                        output: {
+                            title: checkName,
+                            summary: summary,
+                            text: text,
+                        },
+                    };
+                    _b.label = 1;
+                case 1:
+                    _b.trys.push([1, 3, , 4]);
+                    return [4 /*yield*/, octokit.rest.checks.create(checkParams)];
+                case 2:
+                    _b.sent();
+                    return [3 /*break*/, 4];
+                case 3:
+                    error_2 = _b.sent();
+                    (0, core_1.setFailed)("Failed to create check: " + error_2);
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+var postComment = function (outputMd) { return __awaiter(void 0, void 0, void 0, function () {
+    var context, getOctokit, octokit, _a, owner, repo, pullRequest, isFork, prNumber, botComment, data, data, error_3;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _b.trys.push([0, 6, , 7]);
+                context = github.context, getOctokit = github.getOctokit;
+                octokit = getOctokit(process.env.GITHUB_TOKEN);
+                _a = context.repo, owner = _a.owner, repo = _a.repo;
+                pullRequest = context.payload.pull_request;
+                if (!pullRequest) {
+                    console.log("Skipping since this is not a pull request");
+                    process.exit(0);
+                }
+                isFork = pullRequest.head.repo.fork;
+                prNumber = pullRequest.number;
+                if (isFork) {
+                    (0, core_1.setFailed)("The action could not create a Github comment because it is initiated from a forked repo. View the action logs for a list of broken links.");
+                    return [2 /*return*/, ""];
+                }
+                return [4 /*yield*/, findBotComment({
+                        octokit: octokit,
+                        owner: owner,
+                        repo: repo,
+                        prNumber: prNumber,
+                    })];
+            case 1:
+                botComment = _b.sent();
+                if (!botComment) return [3 /*break*/, 3];
+                return [4 /*yield*/, octokit.rest.issues.updateComment({
+                        owner: owner,
+                        repo: repo,
+                        comment_id: botComment === null || botComment === void 0 ? void 0 : botComment.id,
+                        body: outputMd,
+                    })];
+            case 2:
+                data = (_b.sent()).data;
+                return [2 /*return*/, data.html_url];
+            case 3: return [4 /*yield*/, octokit.rest.issues.createComment({
+                    owner: owner,
+                    repo: repo,
+                    issue_number: prNumber,
+                    body: outputMd,
+                })];
+            case 4:
+                data = (_b.sent()).data;
+                return [2 /*return*/, data.html_url];
+            case 5: return [3 /*break*/, 7];
+            case 6:
+                error_3 = _b.sent();
+                (0, core_1.setFailed)("Error updating comment: " + error_3);
+                return [2 /*return*/, ""];
+            case 7: return [2 /*return*/];
+        }
+    });
+}); };
+var generateOutputMd = function (output) {
+    var outputMd = "".concat(COMMENT_TAG);
+    var linksByPage = output.links.reduce(function (acc, link) {
+        if (!acc[link.base.resolved]) {
+            acc[link.base.resolved] = [];
+            acc[link.base.resolved].push(link);
+        }
+        else {
+            acc[link.base.resolved].push(link);
+        }
+        return acc;
+    }, {});
+    Object.entries(linksByPage).forEach(function (_a) {
+        var page = _a[0], links = _a[1];
+        var pageBasePath = new URL(page).pathname;
+        outputMd += "\n\n### `".concat(page, "`\n\n|     | link | text | line |\n|-----|------|------|----------|");
+        // @ts-expect-error
+        links.forEach(function (link) {
+            outputMd += "\n| [ ] | ".concat(link.url.resolved, " | ").concat(link.html.text
+                .trim()
+                .replaceAll("\n", ""), " | `").concat(pageBasePath, ":").concat(link.html.location.line, "` |");
+        });
+    });
+    if (output.errors.length) {
+        outputMd += "\n### Errors\n";
+        output.errors.forEach(function (error) {
+            outputMd += "\n".concat(error, "\n");
+        });
+    }
+    return outputMd;
+};
 // Main function that triggers link validation across .mdx files
 function brokenLinkChecker() {
     var _a;
     return __awaiter(this, void 0, void 0, function () {
         var siteUrl, output, options, siteChecker;
+        var _this = this;
         return __generator(this, function (_b) {
             siteUrl = (_a = process.env.VERCEL_PREVIEW_URL) !== null && _a !== void 0 ? _a : "https://authjs-nextra-docs.vercel.app";
             output = {
@@ -57,30 +240,35 @@ function brokenLinkChecker() {
                 excludeExternalLinks: true,
                 excludedKeywords: [],
             };
-            siteChecker = new SiteChecker(options)
-                .on("error", function (error) {
-                // console.log({ error });
-                output.errors.push(error);
-            })
-                .on("link", function (result, customData) {
-                // console.log({ result, customData });
-                if (result.broken && result.reason !== reasons.HTTP_404) {
-                    output.links.push(result);
-                }
-            })
-                .on("page", function (error, pageURL, customData) {
-                // console.log({ error, pageURL, customData });
-                output.pages.push(error);
-            })
-                .on("site", function (error, siteURL, customData) {
-                // console.log({ error, siteURL, customData });
-                output.sites.push(error);
-            })
-                .on("end", function () {
-                // console.log({ output });
-                if (output.links.length) {
-                    setFailed("Found broken links");
-                }
+            siteChecker = new broken_link_checker_1.default.SiteChecker(options, {
+                error: function (error) {
+                    output.errors.push(error);
+                },
+                link: function (result) {
+                    if (result.broken && result.brokenReason === "HTTP_404") {
+                        // console.log({ result, customData });
+                        output.links.push(result);
+                    }
+                },
+                end: function () { return __awaiter(_this, void 0, void 0, function () {
+                    var outputMd, commentUrl;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                if (!output.links.length) return [3 /*break*/, 3];
+                                outputMd = generateOutputMd(output);
+                                return [4 /*yield*/, postComment(outputMd)];
+                            case 1:
+                                commentUrl = _a.sent();
+                                return [4 /*yield*/, updateCheckStatus(commentUrl)];
+                            case 2:
+                                _a.sent();
+                                (0, core_1.setFailed)("Found broken links");
+                                _a.label = 3;
+                            case 3: return [2 /*return*/];
+                        }
+                    });
+                }); },
             });
             siteChecker.enqueue(siteUrl);
             return [2 /*return*/];
--- .github/actions/broken-link-checker/src/index.ts
+++ .github/actions/broken-link-checker/src/index.ts
@@ -1,8 +1,7 @@
-//// @ts-expect-error
-// import { reasons, SiteChecker } from "broken-link-checker";
-// import { setFailed } from "@actions/core";
-const { SiteChecker } = require("broken-link-checker");
-const { setFailed } = require("@actions/core");
+// @ts-expect-error
+import blc from "broken-link-checker";
+import { setFailed } from "@actions/core";
+import * as github from "@actions/github";
 
 type TODO = any;
 type Output = {
@@ -10,6 +9,161 @@ type Output = {
   links: any[];
   pages: any[];
   sites: any[];
+};
+type Comment = {
+  id: number;
+};
+type FindBotComment = {
+  octokit: TODO;
+  owner: string;
+  repo: string;
+  prNumber: number;
+};
+
+const COMMENT_TAG = "## Broken Links";
+
+async function findBotComment({
+  octokit,
+  owner,
+  repo,
+  prNumber,
+}: FindBotComment): Promise<Comment | undefined> {
+  try {
+    const { data: comments } = await octokit.rest.issues.listComments({
+      owner,
+      repo,
+      issue_number: prNumber,
+    });
+
+    return comments.find((c: TODO) => c.body?.includes(COMMENT_TAG));
+  } catch (error) {
+    setFailed("Error finding bot comment: " + error);
+    return undefined;
+  }
+}
+
+async function updateCheckStatus(commentUrl?: string): Promise<void> {
+  const checkName = "Broken Link Checker";
+  const summary =
+    "This PR introduces broken links to the docs. Click details for a list.";
+  const text = `[See the comment for details](${commentUrl})`;
+  const { context, getOctokit } = github;
+  const octokit = getOctokit(process.env.GITHUB_TOKEN!);
+  const { owner, repo } = context.repo;
+  const pullRequest = context.payload.pull_request;
+  const sha = pullRequest?.head.sha;
+
+  const checkParams = {
+    owner,
+    repo,
+    name: checkName,
+    head_sha: sha,
+    status: "completed" as const,
+    conclusion: "failure" as const,
+    output: {
+      title: checkName,
+      summary: summary,
+      text: text,
+    },
+  };
+
+  try {
+    await octokit.rest.checks.create(checkParams);
+  } catch (error) {
+    setFailed("Failed to create check: " + error);
+  }
+}
+
+const postComment = async (outputMd: string) => {
+  try {
+    const { context, getOctokit } = github;
+    const octokit = getOctokit(process.env.GITHUB_TOKEN!);
+    const { owner, repo } = context.repo;
+    const pullRequest = context.payload.pull_request;
+    if (!pullRequest) {
+      console.log("Skipping since this is not a pull request");
+      process.exit(0);
+    }
+    const isFork = pullRequest.head.repo.fork;
+    const prNumber = pullRequest.number;
+    if (isFork) {
+      setFailed(
+        "The action could not create a Github comment because it is initiated from a forked repo. View the action logs for a list of broken links."
+      );
+      return "";
+    }
+
+    const botComment = await findBotComment({
+      octokit,
+      owner,
+      repo,
+      prNumber,
+    });
+    if (botComment) {
+      const { data } = await octokit.rest.issues.updateComment({
+        owner,
+        repo,
+        comment_id: botComment?.id,
+        body: outputMd,
+      });
+
+      return data.html_url;
+    } else {
+      const { data } = await octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: outputMd,
+      });
+      return data.html_url;
+    }
+  } catch (error) {
+    setFailed("Error updating comment: " + error);
+    return "";
+  }
+};
+
+const generateOutputMd = (output: Output): string => {
+  let outputMd = `${COMMENT_TAG}`;
+  const linksByPage = output.links.reduce((acc, link) => {
+    if (!acc[link.base.resolved]) {
+      acc[link.base.resolved] = [];
+      acc[link.base.resolved].push(link);
+    } else {
+      acc[link.base.resolved].push(link);
+    }
+    return acc;
+  }, {});
+  Object.entries(linksByPage).forEach(([page, links]) => {
+    const pageBasePath = new URL(page).pathname;
+    outputMd += `
+
+### \`${page}\`
+
+|     | link | text | line |
+|-----|------|------|----------|`;
+    // @ts-expect-error
+    links.forEach((link: TODO) => {
+      outputMd += `
+| [ ] | ${link.url.resolved} | ${link.html.text
+        .trim()
+        .replaceAll("\n", "")} | \`${pageBasePath}:${
+        link.html.location.line
+      }\` |`;
+    });
+  });
+
+  if (output.errors.length) {
+    outputMd += `
+### Errors
+`;
+    output.errors.forEach((error) => {
+      outputMd += `
+${error}
+`;
+    });
+  }
+  return outputMd;
 };
 
 // Main function that triggers link validation across .mdx files
@@ -28,28 +182,29 @@ async function brokenLinkChecker(): Promise<void> {
     excludedKeywords: [],
   };
 
-  const siteChecker = new SiteChecker(options, {
+  const siteChecker = new blc.SiteChecker(options, {
     error: (error: TODO) => {
-      // console.log({ error });
       output.errors.push(error);
     },
-    link: (result: TODO, customData: TODO) => {
+    link: (result: TODO) => {
       if (result.broken && result.brokenReason === "HTTP_404") {
-        console.log({ result, customData });
+        // console.log({ result, customData });
         output.links.push(result);
       }
     },
-    page: (error: TODO, pageURL: TODO, customData: TODO) => {
-      // console.log({ error, pageURL, customData });
-      output.pages.push(error);
-    },
-    site: (error: TODO, siteURL: TODO, customData: TODO) => {
-      // console.log({ error, siteURL, customData });
-      output.sites.push(error);
-    },
-    end: () => {
-      console.log({ output });
+    end: async () => {
+      // console.log({
+      //   output,
+      //   link0: output.links[0],
+      //   link0Html: output.links[0].html,
+      //   page0: output.pages[0],
+      // });
       if (output.links.length) {
+        const outputMd = generateOutputMd(output);
+        // console.log(outputMd);
+        const commentUrl = await postComment(outputMd);
+
+        await updateCheckStatus(commentUrl);
         setFailed(`Found broken links`);
       }
     },
